dd7916358f1657263a76c80e1ae6bdadebafa239
MinaTower <116684345+MinaTower@users.noreply.github.com>
2023-10-02 22:27:25 +0300

    EOS1
    Objects & scripts

---
+++ Mech0-ZemtsevaA/Assets/CubeAct.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class CubeAct : MonoBehaviour
+{
+    public GameObject Button;
+    public GameObject Cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log('1');
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))//Input.GetKey(KeyCode.Mouse0)
+        {
+            if (Cube.activeInHierarchy)
+            {
+                Cube.SetActive(false);
+            }
+            else
+            {
+                Cube.SetActive(true);
+            }
+        };
+    }
+    /*public void OnClick()
+    {
+        if (Cube.activeInHierarchy)
+        {
+            Cube.SetActive(false);
+        }
+        else
+        {
+            Cube.SetActive(true);
+        }
+    }*/
+}
